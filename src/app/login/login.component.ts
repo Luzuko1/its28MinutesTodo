226f5954f438e1af85a3dc31c01fe32f6c27c71e
Luzuko1 <Luzukondl@gmail.com>
2019-10-29 15:14:43 +0200

    login and error message

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  username = 'Luzuko';
+  password = 'password';
+  errorMessage = 'Invalid Credentials';
+  invalidLogin = false;
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  handleLogin() {
+    // console.log(this.username);
+    if (this.username === 'Luzuko' && this.password === 'password') {
+      // Redirect to welcome page
+      // this.router.navigate()
+      this.invalidLogin = false;
+    } else {
+      this.invalidLogin = true;
+    }
+  }
+}
