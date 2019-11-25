6c20ed62eb155a83a069b629cc27ce57e48783d4
Luzuko1 <Luzukondl@gmail.com>
2019-11-25 15:29:12 +0200

    added a couple of components(logout), Added more components and connected to backend.

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
+import {WelcomeDataService} from '../service/data/welcome-data.service';
+import {error} from 'util';
 
 @Component({
   selector: 'app-welcome',
@@ -10,9 +12,12 @@ import {ActivatedRoute} from '@angular/router';
 
 export class WelcomeComponent implements OnInit {
   message = 'Some welcome message';
+  welcomeMessageFromService: string;
   name = '';
   // Activated Route
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute,
+              private service: WelcomeDataService
+  ) { }
 
   ngOnInit() {
     console.log(this.message);
@@ -20,4 +25,30 @@ export class WelcomeComponent implements OnInit {
     this.name = this.route.snapshot.params.name;
   }
 
+  getWelcomeMessage() {
+  // console.log(this.service.executeHelloWorldBeanService());
+    this.service.executeHelloWorldBeanService().subscribe(
+      response => this.handleSuccessfulResponse(response),
+      error => this.handleErrorResponse(error)
+    );
+   console.log('last line of get message');
+  }
+  getWelcomeMessageWithParameter() {
+    this.service.executeHelloWorldServiceWithPathVariable(this.name).subscribe(
+      response => this.handleSuccessfulResponse(response),
+      error => this.handleErrorResponse(error)
+    );
+  }
+   handleSuccessfulResponse(response) {
+    this.welcomeMessageFromService = response.message;
+     // console.log(response);
+     // console.log(response.message);
+   }
+
+  handleErrorResponse(error) {
+    console.log(error);
+    console.log(error.message);
+    console.log(error.eror);
+    this.welcomeMessageFromService = error.message;
+  }
 }
